9059f12f03dfe21aa1c38e9b401835eca981d20f
jxbbb <jinbu18@mails.ucas.ac.cn>
2022-03-21 19:43:32 +0800

    stash

--- go.sh
+++ go.sh
@@ -5,11 +5,11 @@
         # --output_dir results/test/scene0002_00/image_text_prompt/brick_floor_iter3000 \
 
 
-CUDA_VISIBLE_DEVICES=3 python sem_seg_main.py \
+CUDA_VISIBLE_DEVICES=3 python -m pdb sem_seg_main.py \
 \
         --obj_path scene0055_00 \
-        --label 2 14 4 \
-        --prompt "marble floor, wooden desk, steel bed" \
+        --label 14 2 4 \
+        --prompt "wooden desk, marble floor, steel bed" \
         --output_dir results/augs_comp/scene0055_00/n_1_a_1 \
         --forbidden "human face,English alphabet,lighting,human" \
 \
@@ -20,12 +20,11 @@ CUDA_VISIBLE_DEVICES=3 python sem_seg_main.py \
         --frontview_elev_std 0.01 \
         --frontview_azim_std 0.1 \
         --background 0.1 0.1 0.1 \
-        --with_prior_color \
-        --render_one_grad_one \
+        --render_all_grad_one \
 \
         --n_normaugs 1 \
         --n_augs 1 \
-        --n_views 8 \
+        --n_views 4 \
         --mincrop 0.75 \
         --maxcrop 1.0 \
         --view_min 0.2 \
@@ -38,8 +37,9 @@ CUDA_VISIBLE_DEVICES=3 python sem_seg_main.py \
 \
         --clipavg \
         --sv_stat_loss_weight 0.1 \
-        --report_step 10 
-        
+        --report_step 100 
+
+
         # --image star.jpg \
         # --fixed_all \
         # --rand_background \
